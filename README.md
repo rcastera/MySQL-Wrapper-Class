767434f336e0e381cab9120fee97d85598dfd2df
Richard Castera <richard.castera@gmail.com>
2013-09-27 19:35:09 -0400

    Update for psr-0, add composer

--- README.md
+++ README.md
@@ -1,74 +1,125 @@
 MySQL Abstraction Class
 =============
 
-This is a class that I've used in some of my projects for years. It's a simple wrapper to MySQL written in PHP5 that allows you to connect, run queries and get results. 
-It's been used in production sites for some time now and I've re-factored it several times.
+This is a class that I've used in some of my projects for years. It's a simple wrapper to MySQL
+written in PHP5 that allows you to connect, run queries and get results. It's been used in production
+sites for some time now and I've re-factored it several times.
+
+### Setup
+-----------------
+ Add a `composer.json` file to your project:
+
+```javascript
+{
+  "require": {
+      "rcastera/mysql-wrapper": "v1.0.0"
+  }
+}
+```
+
+Then provided you have [composer](http://getcomposer.org) installed, you can run the following command:
+
+```bash
+$ composer.phar install
+```
+
+That will fetch the library and its dependencies inside your vendor folder. Then you can add the following to your
+.php files in order to use the library (if you don't already have one).
+
+```php
+require 'vendor/autoload.php';
+```
+
+Then you need to `use` the relevant class, and instantiate the class. For example:
 
 
-Examples
------------
-    Select all records from contacts table.
-    <?php
-      require_once('Class.Database.php');
-      $Db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
-    ?>
-    <?php $contacts = $Db->executeQuery('SELECT * FROM contacts')->asObject(); ?>
-      <?php if ($contacts): ?>
-        <ul>
-          <?php foreach($contacts as $contact): ?>
-            <li><?php echo $contact->first_name; ?> <?php echo $contact->last_name; ?></li>
-          <?php endforeach; ?>
-        </ul>
-      <?php else: ?>
-      <p>No contacts found.</p>
-      <?php endif; ?>
-    <?php unset($Db); ?>
+### Getting Started
+-----------------
+```php
+require 'vendor/autoload.php';
 
-    Insert a record into the contacts table.
-    <?php
-      require_once('Class.Database.php');
-      $Db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
-    ?>
-    <?php $inserted = $Db->executeQuery('INSERT INTO contacts (first_name, last_name, email) VALUES ("Isabella", "Castera", "email@domain.com")')->wasInserted(); ?>
-      <?php if ($inserted): ?>
-        <p>Contact Isabella inserted with id, <?php echo $inserted; ?></p>
-      <?php else: ?>
-        <p>No contacts found.</p>
-      <?php endif; ?>
-    <?php unset($Db); ?>
+use rcastera\Mysql\Database;
 
-    Delete a record from the contacts table.
-    <?php
-      require_once('Class.Database.php');
-      $Db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
-    ?>
-    <?php $deleted = $Db->executeQuery('DELETE FROM contacts WHERE first_name = "Isabella"')->wasDeleted(); ?>
-      <?php if ($deleted): ?>
-        <p>Contact Isabella deleted.</p>
-      <?php else: ?>
-        <p>No contacts found.</p>
-      <?php endif; ?>
-    <?php unset($Db); ?>
+$db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
+```
 
-    Update a record in the contacts table.
-    <?php
-      require_once('Class.Database.php');
-      $Db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
-    ?>
-    <?php $updated = $Db->executeQuery('UPDATE contacts SET last_name = "Branson" WHERE first_name = "Richard"')->wasUpdated(); ?>
-      <?php if ($updated): ?>
-        <p>Contact Richard updated.</p>
-      <?php else: ?>
-        <p>No contacts found.</p>
-      <?php endif; ?>
-    <?php unset($Db); ?>
-      
-Contributing
-------------
 
+### Examples
+-----------------
+
+##### Select all records from contacts table.
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Mysql\Database;
+    $db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
+?>
+<?php $contacts = $db->executeQuery('SELECT * FROM contacts')->asObject(); ?>
+<?php if ($contacts): ?>
+<ul>
+    <?php foreach($contacts as $contact): ?>
+    <li><?php echo $contact->first_name; ?> <?php echo $contact->last_name; ?></li>
+    <?php endforeach; ?>
+</ul>
+<?php else: ?>
+<p>No contacts found.</p>
+<?php endif; ?>
+<?php unset($db); ?>
+```
+
+##### Insert a record into the contacts table.
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Mysql\Database;
+    $db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
+?>
+<?php $inserted = $db->executeQuery('INSERT INTO contacts (first_name, last_name, email) VALUES ("Isabella", "Castera", "email@domain.com")')->wasInserted(); ?>
+    <?php if ($inserted): ?>
+    <p>Contact Isabella inserted with id, <?php echo $inserted; ?></p>
+    <?php else: ?>
+    <p>No contacts found.</p>
+    <?php endif; ?>
+<?php unset($db); ?>
+```
+
+##### Delete a record from the contacts table.
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Mysql\Database;
+    $db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
+?>
+<?php $deleted = $db->executeQuery('DELETE FROM contacts WHERE first_name = "Isabella"')->wasDeleted(); ?>
+    <?php if ($deleted): ?>
+    <p>Contact Isabella deleted.</p>
+    <?php else: ?>
+    <p>No contacts found.</p>
+    <?php endif; ?>
+<?php unset($db); ?>
+```
+
+##### Update a record in the contacts table.
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Mysql\Database;
+    $db = new Database('localhost', 'DATABASE', 'USERNAME', 'PASSWORD');
+?>
+<?php $updated = $db->executeQuery('UPDATE contacts SET last_name = "Branson" WHERE first_name = "Richard"')->wasUpdated(); ?>
+    <?php if ($updated): ?>
+    <p>Contact Richard updated.</p>
+    <?php else: ?>
+    <p>No contacts found.</p>
+    <?php endif; ?>
+<?php unset($db); ?>
+```
+
+### Contributing
+-----------------
 1. Fork it.
 2. Create a branch (`git checkout -b my_branch`)
 3. Commit your changes (`git commit -am "Added something"`)
 4. Push to the branch (`git push origin my_branch`)
-5. Create an [Issue][1] with a link to your branch
+5. Create an Issue with a link to your branch
 6. Enjoy a refreshing Coke and wait
---
+++ src/rcastera/Mysql/Database.php
@@ -0,0 +1,354 @@
+<?php
+/**
+ * Copyright (c) 2010 Richard Castera
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without li`ation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace rcastera\Mysql;
+
+class Database
+{
+    /**
+     * Link id to current connection.
+     *
+     * @var integer
+     */
+    private $linkId;
+
+    /**
+     * Result from the last query executed.
+     *
+     * @var integer
+     */
+    private $queryResult;
+
+    /**
+     * First word in a query (ex: SELECT, INSERT, UPDATE, DELETE).
+     *
+     * @var string
+     */
+    private $queryType;
+
+    /**
+     * Last insert id.
+     *
+     * @var integer
+     */
+    private $lastInsertId;
+
+    /**
+     * Constructor.
+     *
+     * @param string $host - host to connect to.
+     * @param string $database - database to connect to.
+     * @param string $username - username of the db to connect to.
+     * @param string $password - password of the db to connect to.
+     * @param boolean $persistent - persistent connection or not.
+     */
+    public function __construct($host = 'localhost', $database = '', $username = '', $password = '', $persistent = false)
+    {
+        if (empty($database) && empty($username) && empty($password)) {
+            throw new \Exception('Missing parameter values to establish connection.');
+        }
+
+        if (! $this->connect($host, $database, $username, $password, $persistent)) {
+            throw new \Exception('Could not establish a connection to Mysql.');
+        }
+    }
+
+    /**
+     * Destructor - Disconnects from the database.
+     */
+    public function __destruct()
+    {
+        if ($this->linkId) {
+            mysql_close($this->linkId);
+        }
+        unset($this);
+    }
+
+    /**
+     * Establishes a connection to the database specified.
+     *
+     * @param string $host - host to connect to.
+     * @param string $database - database to connect to.
+     * @param string $username - username of the db to connect to.
+     * @param string $password - password of the db to connect to.
+     * @param boolean $persistent - persistent connection or not.
+     *
+     * @return boolean
+     */
+    private function connect($host, $database, $username, $password, $persistant)
+    {
+        if (empty($this->linkId)) {
+            if ($persistant) {
+                $this->linkId = mysql_pconnect($host, $username, $password, false);
+            } else {
+                $this->linkId = mysql_connect($host, $username, $password, false);
+            }
+
+            if (! is_resource($this->linkId)) {
+                return false;
+            }
+
+            // If we couldn't select the database, return false.
+            if (! $this->selectDb($database)) {
+                throw new \Exception('Could not connect to database.');
+                return false;
+            } else { // Connection was a success.
+                return true;
+            }
+        } else { // Assume we already have a connection.
+            return true;
+        }
+    }
+
+    /**
+     * Selects the database.
+     *
+     * @param string $database - database to connect to.
+     *
+     * @return boolean
+     */
+    private function selectDb($database)
+    {
+        if (! mysql_select_db($database, $this->linkId)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Retrieves the last error.
+     *
+     * @return string message from the last MySQL function, or empty string if no error occurred.
+     */
+    public function getError()
+    {
+        return mysql_error($this->linkId);
+    }
+
+    /**
+     * Executes a command on the database.
+     *
+     * @param string $sql - query to run.
+     *
+     * @return this.
+     */
+    public function executeQuery($sql = '')
+    {
+        // Check to see that the parameters are not empty.
+        if (! empty($sql)) {
+            $this->runQuery($sql);
+            return $this;
+        } else {
+            throw new \Exception('You need to provide a query.');
+        }
+    }
+
+    /**
+     * Executes a sql query.
+     *
+     * @param string $query - sql statement.
+     *
+     * @return boolean
+     */
+    private function runQuery($query = null)
+    {
+        if (! is_null($query)) {
+            // Determine the query type. (SELECT, UPDATE, INSERT, DELETE etc.)
+            $this->queryType = $this->getQueryType($query);
+
+            if (! $this->queryResult = mysql_query($query, $this->linkId)) {
+                throw new \Exception('Error in sql query.');
+            }
+
+            // Check if the query is an insert. If it is, get the id.
+            if ($this->queryType == 'INSERT') {
+                $this->lastInsertId = mysql_insert_id();
+            }
+        }
+    }
+
+    /**
+     * Gets the last insert id.
+     *
+     * @return integer id generated for an AUTO_INCREMENT column or false.
+     */
+    public function getLastInsertId()
+    {
+        return $this->lastInsertId;
+    }
+
+    /**
+     * Free result memory.
+     *
+     * @return boolean
+     */
+    private function freeResult()
+    {
+        return mysql_free_result($this->queryResult);
+    }
+
+    /**
+     * To determine the query type used in the query. ex: SELECT, INSERT.
+     * In order to run the getAffectedRows(); function below we need to
+     * determine the query type.
+     *
+     * @param string $query - sql statement.
+     *
+     * @return string
+     */
+    private function getQueryType($query = '')
+    {
+        $query = explode(' ', $query);
+        return strtoupper($query[0]);
+    }
+
+    /**
+     * Verifies that the last query executed successfully.
+     *
+     * @return boolean
+     */
+    public function querySucceeded()
+    {
+        if (! $this->queryResult) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Gets the number of rows affected by the last query executed.
+     *
+     * @return integer
+     */
+    public function getAffectedRows()
+    {
+        if ($this->querySucceeded()) {
+            // Retrieves the number of rows from a result set. This command is only valid
+            // for statements like SELECT or SHOW that return an actual result set.
+            if (($this->queryType == 'SELECT') || ($this->queryType == 'SHOW')) {
+                return mysql_num_rows($this->queryResult);
+            } else {
+                // To retrieve the number of rows affected by a INSERT, UPDATE,
+                // REPLACE or DELETE query, use mysql_affected_rows().
+                return mysql_affected_rows();
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Verifies that the current INSERT sql call ran successfully.
+     *
+     * @return integer
+     */
+    public function wasInserted()
+    {
+        if ($this->queryType == 'INSERT' && $this->querySucceeded()) {
+            return $this->getLastInsertId();
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Verifies that the current UPDATE sql call ran successfully.
+     *
+     * @return boolean
+     */
+    public function wasUpdated()
+    {
+        if ($this->queryType == 'UPDATE' && $this->querySucceeded()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Verifies that the current DELETE sql call ran successfully.
+     *
+     * @return boolean
+     */
+    public function wasDeleted()
+    {
+        if ($this->queryType == 'DELETE' && $this->querySucceeded()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Return the result as an array.
+     *
+     * @return array
+     */
+    public function asArray()
+    {
+        // If the last query ran was unsuccessfull, then return false.
+        if (! $this->queryResult) {
+            return false;
+        } else {
+            if (! $this->getAffectedRows() == 0) {
+                $rows = array();
+
+                while ($row = mysql_fetch_assoc($this->queryResult)) {
+                    array_push($rows, $row);
+                }
+
+                $this->freeResult();
+                return $rows;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Return the result as an object.
+     *
+     * @return array
+     */
+    public function asObject()
+    {
+        // If the last query ran was unsuccessfull, then return false.
+        if (! $this->queryResult) {
+            return false;
+        } else {
+            if (! $this->getAffectedRows() == 0) {
+                $rows = array();
+
+                while ($row = mysql_fetch_object($this->queryResult)) {
+                    array_push($rows, $row);
+                }
+
+                $this->freeResult();
+                return $rows;
+            } else {
+                return false;
+            }
+        }
+    }
+}
